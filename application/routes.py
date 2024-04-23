3f8e52e058a55b1d2505f8d4d2960b2e5fb6710f
mac <mac@Alis-MacBook-Air.local>
2024-04-23 07:02:55 +0200

    Editing routes.py

--- application/routes.py
+++ application/routes.py
@@ -2,7 +2,7 @@ from application import app
 from flask import render_template
 
 @app.route("/")
-@app.route("/login")
+# @app.route("/login")
 def login():
     return render_template("login-test.html")
 
