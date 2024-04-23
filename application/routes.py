a4b638b065c72b33b68e23831d6f854838d9cd7b
mac <mac@Alis-MacBook-Air.local>
2024-04-23 07:16:22 +0200

    Changing the host and port -test

--- application/routes.py
+++ application/routes.py
@@ -2,7 +2,10 @@ from application import app
 from flask import render_template
 
 @app.route("/")
-# @app.route("/login")
+def test():
+    return "Helthy"
+
+@app.route("/login")
 def login():
     return render_template("login-test.html")
 
