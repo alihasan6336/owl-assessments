72449acc3669017613dda02944ee1c44b6aae428
mac <mac@Alis-MacBook-Air.local>
2024-04-23 07:55:48 +0200

    Returning tha project to it's original form

--- application/routes.py
+++ application/routes.py
@@ -2,13 +2,10 @@ from application import app
 from flask import render_template
 
 @app.route("/")
-def test():
-    return "Helthy"
-
 @app.route("/login")
 def login():
     return render_template("login-test.html")
 
-@app.route("/register"  )
+@app.route("/register")
 def register():
     return render_template("register-test.html")
