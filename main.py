2904f0890150687d45fd98d0dad97b9566f85a88
mac <mac@Alis-MacBook-Air.local>
2024-04-23 07:39:25 +0200

    testing trivial app

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from application import app
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'OK', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
