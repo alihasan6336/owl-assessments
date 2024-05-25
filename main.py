1970d2dffecd3c69d39d83dd824d4c178bdea3ea
alihasan6336 <mac@Alis-MacBook-Air.local>
2024-05-25 05:29:35 +0300

    Updating companies register & databse handling & 90% form validations

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
 from application import app, db
 
-with app.app_context():
-    db. create_all()
+try:
+    with app.app_context():
+        db.create_all()
+except Exception as e:
+    logging.exception("Failed to create tables")
