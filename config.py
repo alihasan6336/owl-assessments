dad2a4a025af97fbe08c6e5c50c80f7219b5e4ba
alihasan6336 <mac@Alis-MacBook-Air.local>
2024-05-25 23:25:04 +0300

    Updating config.py final

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 class Config():
     SECRET_KEY = os.environ.get("SECRET_KEY") or "secret_key_123"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL_TEST") or "postgresql://postgres:postgresadmin@localhost:5432/flask_db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "postgresql://postgres:postgresadmin@localhost:5432/flask_db"
