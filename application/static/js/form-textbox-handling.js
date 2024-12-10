87d7578b16cb903c60f09dad595f551a67f7b0ab
alihasan6336 <mac@Alis-MacBook-Air.local>
2024-12-10 19:05:47 +0200

    editing form-textbox-handling.js

--- application/static/js/form-textbox-handling.js
+++ application/static/js/form-textbox-handling.js
@@ -1,15 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const inputs = document.querySelectorAll(".form-control");
-  
-  inputs.forEach(input => {
-      const label = input.nextElementSibling;
-      
-      input.addEventListener("input", function() {
-          if (this.value.trim() !== "") {
-              label.classList.add("active");
-          } else {
-              label.classList.remove("active");
-          }
-      });
-  });
+    const inputs = document.querySelectorAll(".form-control");
+    
+    function handleInputChange(event) {
+        const input = event.target.value;
+        const label = event.target.nextElementSibling;
+        
+        if (input.trim() !== "") {
+            label.classList.add("active");
+        } else {
+            label.classList.remove("active");
+        }
+    }
+
+    inputs.forEach(input => {
+        handleInputChange({ target: input });
+        input.addEventListener("input", handleInputChange);
+    });
 });
